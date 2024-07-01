5885623fa0e70db6ff2cc557da76a2eb6dfc878a
Awaisee01 <awaisasif096@gmail.com>
2024-07-01 06:15:28 -0700

    Commit message

---
+++ oo.js
@@ -0,0 +1,83 @@
+// Write a JavaScript program that generates a number between 1 and 10 (inclusive). The program should then prompt the user to guess the number. Use a do-while loop to continue prompting the user until they guess the correct number. Provide feedback on whether their guess is too high, too low, or correct.
+
+// const x = Math.floor(Math.random()*10)
+// console.log(x)
+// let y;
+// do {
+//     y = parseInt(prompt('guess the number between 1 and 10'))
+//     if (y < x) {
+//         alert('too low try again')
+
+//     }
+//     else if (y > x) {
+//         alert('too high try again')
+
+//     }
+//     else if ( y == x) {
+//         alert('congragulation')
+//     }
+    
+    
+// }
+// while(y !== x)
+
+
+// You are developing an authentication system for a website. Write a JavaScript code snippet that prompts the user to input a username and password. The system should then check if the provided username and password match the correct values. The correct username is "user123" and the correct password is "Passw0rd".    Use   do-while lopp
+
+
+// let userName = "user123"
+// let passWord = 'passw0rd'
+// let userNam
+// let pass
+
+// do {    
+//    userNam = prompt('enter username')
+//    pass = prompt('enter password')
+
+//     if(userNam !== userName || pass !== passWord) {
+//         alert("try again")
+
+//     }
+// }
+// while ( userNam !== userName || pass !== passWord)
+//         alert('congragulation')
+
+
+
+
+// let x = prompt()
+//  for (let i = 1; i <= x; i--){
+//     if(i%17===0)
+// console.log(i)
+//  }
+
+
+
+// let x = "Hello";
+// let y = ""2 !
+// for (let i = x.length - 1; i >= 0; i--) {
+//     y += x[i];
+// }
+// console.log(y);
+
+
+
+// x = promt()
+// for(let i = x; i>=1; i--){
+//     x = x*i
+// }
+// console.log(x)
+
+
+// for( let i = 1; i<=100; i++){
+//     if(i%2 !==0)
+//     console.log(i)
+// }
+
+// let marks = [54, 45, 45, 545, 45]
+// let sum = 0
+// for(let val of marks){
+//     sum+= sum+val
+
+// }
+// console.log(sum)
